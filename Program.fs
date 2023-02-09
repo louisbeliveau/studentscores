f353fde7fb3649aaef4d55c29cb9742647c21741
louis911911 <louis911911@yahoo.com>
2023-02-08 19:44:16 -0500

    organization par fichiers

--- Program.fs
+++ Program.fs
@@ -1,51 +1,7 @@
 ﻿open System
 open System.IO
+open StudentScores    
 
-module Float=
-    let tryFromString s=
-        if s="N/A" then
-            Some(float 50.0)
-        else
-            Some(float s)
-
-    
-type Student=
-    {
-        FName:string
-        LName:string
-        Id:string
-        MeanScore:float
-        MinScore:float
-        MaxScore:float
-    }
-module StudentModule=
-    let fromString(s:string)=
-        let elements = s.Split('\t')
-        let name =elements[0].Split(",")
-        let id=elements[1]
-        let scores =
-            elements
-            |> Array.skip 2
-            |> Array.choose Float.tryFromString
-        let meanScore = scores |> Array.average
-        let minScore = scores |> Array.min
-        let maxScore = scores |> Array.max
-        {             
-        Id=id
-        FName=name[1]
-        LName=name[0]
-        MeanScore=meanScore
-        MinScore=minScore
-        MaxScore=maxScore
-        }
-
-    // let fromStringOr50 s=
-    //     if s=="N/A" then
-    //         Float.tryFromString 
-    //     0
-
-    let printSummary (student:Student)  =
-        printfn "%s\t%s\t%s\t%0.1f\t%0.1f\t%0.1f" student.FName student.LName student.Id student.MeanScore student.MinScore student.MaxScore
 
 let sumarize (filepath:string)=
     let rows = File.ReadAllLines filepath
